cad85f2f3101f262a459a6db8fa7e7dc3da7c73c
Belni22 <100565102+Belni22@users.noreply.github.com>
2024-06-18 09:23:28 +0200

    Changes on app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React, { useState } from 'react';
-import Sidebar from './components/Sidebar';
-import Viewer from './components/Viewer';
 import DicomViewer from './components/DicomViewer';
 import './App.css';
 
 function App() {
-    const [file, setFile] = useState(null);
 
-    const handleFileUpload = (files) => {
-        setFile(files[0]);
-    };
 
     return (
         <div className="app">
-            <Sidebar onFileUpload={handleFileUpload} />
-            <div className="viewer">
-                {file && <Viewer file={file} />}
-            </div>
             <DicomViewer />
         </div>
     );
